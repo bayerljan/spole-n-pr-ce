3dd8400f166005873db1319dd8b0b17cd13b7886
CafourekJ <jan.cafourek@infis.cz>
2025-04-29 15:08:52 +0200

    Update hra.py

--- hra.py
+++ hra.py
@@ -1,5 +1,4 @@
-#29 - 32
-         (hrac == "papír" and pocitac == "kámen"):
+        (hrac == "papír" and pocitac == "kámen"):
         print("Vyhráváš toto kolo!")
         skore_hrac += 1
     else:
