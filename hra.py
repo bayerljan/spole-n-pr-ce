f7c72b775fe9c1145a50ef3a3602a96294b6779a
Brejcha123 <jan.brejcha@infis.cz>
2025-04-29 15:17:40 +0200

    Update hra.py

---
+++ hra.py
@@ -0,0 +1,4 @@
+print("\n==============================")
+print(f"Celkový počet kol: {pocet_kol}")
+print(f"Konečné skóre -> Ty: {skore_hrac} | Počítač: {skore_pocitac}")
+if skore_hrac > skore_pocitac:
