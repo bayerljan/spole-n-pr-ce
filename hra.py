25885d3fb22a9c918239deec6600bebd0e734b69
Martin_Friedel <martin.friedel@infis.cz>
2025-04-29 15:21:21 +0200

    hra.py

--- hra.py
+++ hra.py
@@ -2,9 +2,3 @@
     if hrac not in moznosti:
         print("Neplatná volba. Zkus to znovu.")
         continue
-
-        (hrac == "papír" and pocitac == "kámen"):
-        print("Vyhráváš toto kolo!")
-        skore_hrac += 1
-    else:
-
