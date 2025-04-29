68e2d4647f04af50de3866141703a6b8a177ff0b
jacobjegay <lukas.jirinec@infis.cz>
2025-04-29 15:17:33 +0200

    Update hra.py

--- hra.py
+++ hra.py
@@ -1,4 +1,4 @@
-        (hrac == "papír" and pocitac == "kámen"):
-        print("Vyhráváš toto kolo!")
-        skore_hrac += 1
-    else:
+  print("Počítač vyhrává toto kolo!")
+        skore_pocitac += 1
+    pocet_kol += 1
+    print(f"Aktuální skóre -> Ty: {skore_hrac} | Počítač: {skore_pocitac}")
